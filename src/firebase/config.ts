0f228bebfca93e351b298f2aa94e531a1288fc26
rob <admin@fundingcloud.com>
2025-11-05 10:51:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "heidless-agents",
+  "appId": "1:1020067846179:web:6a16581eb0a60b3490b996",
+  "apiKey": "AIzaSyAwtFMYW8rZadT0aHuSIDF1VaOaCt2n9Ts",
+  "authDomain": "heidless-agents.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1020067846179"
+};
