a681f18ff4abbb214f58342884f71e2b1e02d3b8
rob <admin@fundingcloud.com>
2025-11-05 10:08:40 +0000

    File changes

---
+++ src/lib/firebase/config.ts
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For more information on how to use this object, see the following documentation:
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAwtFMYW8rZadT0aHuSIDF1VaOaCt2n9Ts",
+  appId: "1:1020067846179:web:6a16581eb0a60b3490b996",
+  authDomain: "heidless-agents.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "1020067846179",
+  projectId: "heidless-agents",
+  storageBucket: "heidless-agents.appspot.com",
+};
