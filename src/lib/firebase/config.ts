fc221ef31c8b9a3cb160f2e69938c3d348fd6d7a
rob <admin@fundingcloud.com>
2025-11-05 10:28:04 +0000

    Modify the app so that app environment variables defined in a .env file

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -3,10 +3,10 @@
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 export const firebaseConfig = {
-  "projectId": "heidless-agents",
-  "appId": "1:1020067846179:web:6a16581eb0a60b3490b996",
-  "apiKey": "AIzaSyAwtFMYW8rZadT0aHuSIDF1VaOaCt2n9Ts",
-  "authDomain": "heidless-agents.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1020067846179"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
